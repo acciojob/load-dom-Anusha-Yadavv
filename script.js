6308205c76ab1b3c83394a509c1a3b9575d84fa0
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-19 22:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function() {
+            // DOM has loaded, let's write a message to the web page
+            var messageParagraph = document.createElement('p');
+            messageParagraph.textContent = 'DOM load success';
+            document.body.appendChild(messageParagraph);
+        });
