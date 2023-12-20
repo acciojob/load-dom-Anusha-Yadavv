435fe2b5b4e1a50705aba89d0d82c592c2465cc1
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-20 10:22:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
- document.addEventListener('DOMContentLoaded', function () {
-        // Get the body element
-        var body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+            // Get the body element
+            var bodyElement = document.body;
 
-        // Check if the body is empty
-        if (body.childNodes.length === 0) {
-            // Create a text node with the message
-            var textNode = document.createTextNode('DOM load success');
+            // Ensure that the body is empty
+            bodyElement.innerHTML = "";
+
+            // Create a new text node with the content "DOM load success"
+            var textNode = document.createTextNode("DOM load success");
 
             // Append the text node to the body
-            body.appendChild(textNode);
-        }
-    });
+            bodyElement.appendChild(textNode);
+        });
