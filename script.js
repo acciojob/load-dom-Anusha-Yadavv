17d1d79780b62fd8876ff84c0ac76bc0fce0eb21
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-20 10:17:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
  window.addEventListener('DOMContentLoaded', function() {
-            let pElement = document.createElement('p');
-            pElement.innerText = 'DOM load success';
+            // let pElement = document.createElement('p');
+	 let bodyElement=document.getEelementsByTagName('body')
+           bodyElement.innerText = 'DOM load success';
             document.body.appendChild(pElement);
         });
